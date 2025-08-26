fcae24e38bbb68b6f0d69c7b736a755c2ce9d6bd
Stepan <Stepan.Isakhin@infotecs.ru>
2025-08-26 10:44:22 +0500

    rebuild to fiber

--- app/cmd/app/main.go
+++ app/cmd/app/main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
-	"net/http"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"log"
 	"testCaseGO/internal/handler"
 	"testCaseGO/internal/service"
+	"testCaseGO/internal/setup"
 )
 
 func main() {
-	handler.HandleRequest()
-	service.CreateTables()
-	service.ExistsWallets()
-	err := http.ListenAndServe(":8000", nil)
-	if err != nil {
-		return
+	err := setup.PrepareEnv()
+	{
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-
+	defer func() {
+		service.CloseDB(service.Db)
+		err := recover()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+	app := fiber.New(fiber.Config{})
+	app.Use(logger.New())
+	app.Use(recover())
+	handler.RegisterRoutes(app)
+	if err := app.Listen(":8000"); err != nil {
+		log.Fatal(err)
+	}
 }
